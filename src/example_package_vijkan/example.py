281d2a5f4f557733f03892c9ff1bcf418585ebb4
Vijaikannan <vijaik.nd@gmail.com>
2023-05-04 15:35:37 +0530

    Example Package Creation using Actions

---
+++ src/example_package_vijkan/example.py
@@ -0,0 +1,16 @@
+"""
+Functions returns next number for the given argument
+"""
+
+def add_one(number):
+    """
+    Next Number of given arguments.
+    Keyword arguments:
+    -----------------
+    number : number format.   
+    Returns:
+    --------
+    number+1 : Number.
+        Returns Next Numbers of the given number.
+    """ 
+    return number + 1
